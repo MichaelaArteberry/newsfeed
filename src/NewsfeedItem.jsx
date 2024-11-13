34dd6f39704c967f139d3ff34978a3a876d35160
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-11-13 10:28:40 -0500

    event handler for repost button with alert message added

--- src/NewsfeedItem.jsx
+++ src/NewsfeedItem.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
 
-const NewsfeedItem = ({ message }) => (
+const NewsfeedItem = ({ message }) => {
+
+  const RepostButtonClick = (message) => {
+      alert(`You reposted a message by ${message.username}`)
+  }
+
+  return(
   <div className="newsfeed-item">
-    <h1>@{ message.username }</h1>
-    <span>reposts: { message.reposts }</span>
-    <p>
+      <h1>@{message.username}</h1>
+      <span>reposts: {message.reposts}</span>
+      <p>
       { message.text }
-    </p>
-    <hr></hr>
-  </div>
-)
+      </p>
+      <button onClick={() => RepostButtonClick(message)}> Repost </button>
+      <hr></hr>
+  </div>)
+}
 
 export default NewsfeedItem;
