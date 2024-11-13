32e7d2353f96b7532ca8d132fe891e530f98c656
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-11-13 14:16:59 -0500

    installed bootstrap and added a navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Newsfeed from './Newsfeed.jsx';
+import Navbar from './Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Newsfeed />
     </div>
   );
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => (
+    <div className="navbar">
+        <h1>My Practice Newsfeed App</h1>
+        <ul>
+        <li>Home</li>
+        <li>Profile</li>
+        <li>Messages</li>
+        </ul>
+    </div>
+    )
+
+export default Navbar;
